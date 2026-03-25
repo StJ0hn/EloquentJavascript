95584dea58c36fd372159e949164a42c82603929
john <jonhmiguel138@gmail.com>
2026-03-25 16:43:43 -0300

    feat: exercicio 2 e 3 concluídos

---
+++ capitulo1-ValoresTiposEOperadores/ex3/index.js
@@ -0,0 +1,15 @@
+const TAMANHO_ALTURA = Number(process.argv[2]);
+const TAMANHO_LARGURA = Number(process.argv[3]);
+let sequencia = "";
+for (let i = 1; i < TAMANHO_LARGURA; i++) {
+    for (let j = 0; j < TAMANHO_ALTURA; j++) {
+        if ((i + j) % 2 == 0){
+            sequencia += " ";
+        }
+        else {
+            sequencia += "#";
+        }
+    }
+    sequencia += "\n"
+}
+console.log(sequencia);
